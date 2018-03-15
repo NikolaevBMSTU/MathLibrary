21e17a994f131c34da4195fe710b9733a1b62fd8
NikolaevBMSTU <denn.Nikolaev@gmail.com>
2018-03-15 22:13:07 +0300

    Отлажен и протестирован на некоторых задачах явный метод Эйлера

--- SMAN/EulerODEsolver.cs
+++ SMAN/EulerODEsolver.cs
@@ -1,68 +1,72 @@
 using System;
 
 
- //Compiler version 4.0, .NET Framework 4.5
+//Compiler version 4.0, .NET Framework 4.5
 // есть лишнее повторение, когда требуется удвоить шаг
 
- namespace SMAN
- {
- 	
- 	public class ODE
- 	{
- 		public delegate double FunkXY(double x, double y);
- 		
- 		public static double[] Euler(double y0, double a, double b, int p, FunkXY D)
- 		// Method of Euler
- 		{
- 			int n = (p-1)/2+1;
- 			int n2 = 2*(n-1)+1 ;
- 
- 			double[] y;
- 			double[] y2n = new double[n2];
- 			double dx;
- 			int count = 1;
- 			
- 			bool end = false;
- 			while (!end)
- 			{
- 				count += 1;
- 				n = 2*(n-1)+1 ;
- 				dx = (b - a) / (n);
- 				y = new double[n];
- 				y[0] = y0;
- 				for (int i = 1; i < n; i++)
- 				{
- 					y[i] = y[i - 1] + dx * D(a + dx * i, y[i - 1]);
- 				}
- 				
- 				n2 = 2*(n-1)+1;
- 				dx = (b - a) / n2;
- 				y2n = new double[n2];
- 				y2n[0] = y0;
- 				for (int i = 1; i < n2; i++)
- 				{
- 					y2n[i] = y2n[i - 1] + dx * D(a + dx * i, y2n[i - 1]);
- 				}
- 				
- 				end = true;
- 				for (int i = 0; i < n; i++)
- 				{
- 					end = end && (Math.Abs(y[i] - y2n[2 * i]) / 2 < 1e-4);
- 				}
- 			}
- 			
- 			int k = n2/p;
- 			k = count;
- 			double[] answer = new double[p];
- 			for (int i = 1; i < p; i++)
- 			{
- 				answer[i] = y2n[k*(i)];
- 			}
- 			
- 			return answer;
- 		}
- 	}
- 	
- }
+namespace SMAN
+{
+
+    public class ODE
+    {
+        public delegate double FunkXY(double x, double y);
+
+        public static double[] Euler(double y0, double a, double b, int p, FunkXY D)
+        // Method of Euler
+        {
+            //int n = (p - 1) / 2 + 1;
+            int n = p;
+            int n2 = 2 * (n - 1) + 1;
+
+            double[] y;
+            double[] y2n = new double[n2];
+            double dx = 2 * (b - a) / (n - 1);
+            int count = -1;
+
+            bool end = false;
+            while (!end)
+            {
+                count += 1;
+                dx = dx / 2;
+                n = Convert.ToInt32((b - a) / dx) +1;
+                y = new double[n];
+                y[0] = y0;
+                for (int i = 1; i < n; i++)
+                {
+                    y[i] = y[i - 1] + dx * D(a + dx * i, y[i - 1]);
+                }
+
+                count += 1;
+                dx = dx / 2;
+                n2 = Convert.ToInt32((b - a) / dx)+1;
+                y2n = new double[n2];
+                y2n[0] = y0;
+                for (int i = 1; i < n2; i++)
+                {
+                    y2n[i] = y2n[i - 1] + dx * D(a + dx * i, y2n[i - 1]);
+                }
+
+                end = true;
+                for (int i = 1; i < n; i++)
+                {
+                    end = end && (Math.Abs(y[i] - y2n[2 * i]) / 2 < 1e-4);
+                }
+            }
+
+            int k = Convert.ToInt32(Math.Pow(2, count));
+            //k = count;
+            count = 0;
+            double[] answer = new double[p];
+            for (int i = 1; i < p; i++)
+            {
+                count = count + k;
+                answer[i] = y2n[count];
+            }
+
+            return answer;
+        }
+    }
+
+}
     
     
---
+++ UnitTests/SMAN_UnitTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Vectors;
+using Matrixs;
+using SMAN;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ODEClassTests
+    {
+        public double Const(double x, double y)
+        {
+            return 1;
+        }
+
+        public double Line(double x, double y)
+        {
+            return x;
+        }
+
+        public double Exp(double x, double y)
+        {
+            return Math.Pow(Math.E, x);
+        }
+
+        [TestMethod]
+        public void Euler_Exp()
+        {
+            double expected = 1.7183;
+            double actual = ODE.Euler(0, 0, 1, 10, Exp)[9];
+            Assert.AreEqual(expected, actual, 0.001, "Решение найдено не верно");
+        }
+
+        [TestMethod]
+        public void Euler_Const()
+        {
+            double expected = 1;
+            double actual = ODE.Euler(0, 0, 1, 10, Const)[9];
+            Assert.AreEqual(expected, actual, 0.001, "Решение найдено не верно");
+        }
+
+        [TestMethod]
+        public void Euler_Line()
+        {
+            double expected = 0.5;
+            double actual = ODE.Euler(0, 0, 1, 10, Line)[9];
+            Assert.AreEqual(expected, actual, 0.001, "Решение найдено не верно");
+        }
+
+
+    }
+
+
+}
